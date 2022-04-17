9c3f89de72ae834e7df08b839cc6be5d45366860
Trigun Joshi <trigunjoshi@gmail.com>
2022-04-17 11:52:24 -0700

    custom css module added to meetup detail

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,16 @@
+
+import MeetupDetail from '../../components/meetups/MeetupDetail'
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://images.pexels.com/photos/3719037/pexels-photo-3719037.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      title="A first meetup"
+      address="Vancouver, Downtown"
+      description="Agile Scrum description"
+    />
+
+  )
+}
+
+export default MeetupDetails;
