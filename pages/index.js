41c91fa08e61939a960a783851c02b2accd7af61
Trigun Joshi <trigunjoshi@gmail.com>
2022-04-19 01:10:48 -0700

    changes from mockdata to real time data from database

--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,29 @@ import { MongoClient } from 'mongodb';
 
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'Agile meetup',
-    image: 'https://images.pexels.com/photos/3719037/pexels-photo-3719037.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    address: 'Vancouver, Downtown',
-    description: 'Agile meetup'
-  },
-  {
-    id: 'm2',
-    title: 'Design first meetup',
-    image: 'https://images.pexels.com/photos/1181396/pexels-photo-1181396.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    address: 'Vancouver, Downtown',
-    description: 'Design first meetup'
-  },
-  {
-    id: 'm3',
-    title: 'Product Roadmap meetup',
-    image: 'https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    address: 'Vancouver, Downtown',
-    description: 'Product Roadmap meetup'
-  }
-]
+// const DUMMY_MEETUPS = [
+//   {
+//     id: 'm1',
+//     title: 'Agile meetup',
+//     image: 'https://images.pexels.com/photos/3719037/pexels-photo-3719037.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+//     address: 'Vancouver, Downtown',
+//     description: 'Agile meetup'
+//   },
+//   {
+//     id: 'm2',
+//     title: 'Design first meetup',
+//     image: 'https://images.pexels.com/photos/1181396/pexels-photo-1181396.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+//     address: 'Vancouver, Downtown',
+//     description: 'Design first meetup'
+//   },
+//   {
+//     id: 'm3',
+//     title: 'Product Roadmap meetup',
+//     image: 'https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+//     address: 'Vancouver, Downtown',
+//     description: 'Product Roadmap meetup'
+//   }
+// ]
 const HomePage = (props) => {
 
   return (<MeetupList meetups={props.meetups} />)
@@ -59,7 +59,7 @@ export async function getStaticProps() {
         title: meetup.title,
         address: meetup.address,
         image: meetup.image,
-        description: meetup.description, 
+        description: meetup.description,
         id: meetup._id.toString()
       }))
     },
