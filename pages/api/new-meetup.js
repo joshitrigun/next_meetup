f25237bb08578e081f657b08d6d5e422b0ad648e
Trigun Joshi <trigunjoshi@gmail.com>
2022-04-19 00:53:05 -0700

    database connected with server and post routes initiated

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,23 @@
+import { MongoClient } from 'mongodb';
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+
+
+    const client = await MongoClient.connect('mongodb+srv://Trigun:9841735635aA@cluster0.l11u7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetups inserted' })
+  }
+}
+
+export default handler; 
